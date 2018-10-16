d26b3baebb266a72e546cf86e6e11b29633ba497
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-10-16 13:15:03 -0400

    Updated from Brython Server: 10/16/2018 1:15:03 PM

--- cryptography.py
+++ cryptography.py
@@ -18,14 +18,14 @@ if command == "e" or command == "d":
     nums = []
     if command == "e":
         for i in range(len(message)):
-        encryptnumb = (associations.find(message[i]) + associations.find(key[i]))
-        nums.append(encryptnumb)
+            encryptnumb = (associations.find(message[i]) + associations.find(key[i]))
+            nums.append(encryptnumb)
         for x in nums:
             print(associations[x], end = '')
     if command == "d":
         for i in range(len(message)):
-        decryptnumb = (encryptnumb - associations.find(key[i]))
-        nums.append(decryptnumb)
+            decryptnumb = (encryptnumb - associations.find(key[i]))
+            nums.append(decryptnumb)
         for x in nums:
             print(associations[x], end = '')
     
